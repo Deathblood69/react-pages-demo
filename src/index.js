c48d2479cff5279264c755ac66d9f6750d616e51
Deathblood69 <deathblood69002@gmail.com>
2022-11-27 09:29:14 +0100

    Correction test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./css/index.css";
 import App from "./pages/App";
 import reportWebVitals from "./fonctions/reportWebVitals";
 
