b59ec9ca4708e58f9fe3bdc0982d8ab2cca25542
Deathblood69 <deathblood69002@gmail.com>
2022-11-27 09:34:10 +0100

    Test Signed commit

--- src/pages/App.js
+++ src/pages/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload with Romain D.
+          Edit <code>src/App.js</code> and save to reload with Romain DIASPARRA.
         </p>
         <a
           className="App-link"
