bca0406f0b39b08b1c34fcca1e783ac221c234ec
Deathblood69 <deathblood69002@gmail.com>
2022-11-27 10:26:31 +0100

    Update main.yml

--- src/pages/App.js
+++ src/pages/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Modifier <code>src/pages/App.js</code> et sauvegarder pour actualiser
+          la page.
         </p>
         <a
           className="App-link"
